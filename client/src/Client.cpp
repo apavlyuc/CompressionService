b17da6a591f01fc1e5e6ed0cc42a939545bc9184
apavlyuc <apavlyuc@gmail.com>
2020-06-19 01:05:28 +0300

    implement all requests; add function for header creation

--- client/src/Client.cpp
+++ client/src/Client.cpp
@@ -1,6 +1,39 @@
 #include "Client.hpp"
 
 #include <iostream>
+#include <cstring> // memcpy, memset
+#include <string_view>
+
+namespace {
+	enum class RequestType
+	{
+		ping = 1,
+		get_stats = 2,
+		reset_stats = 3,
+		compress = 4
+	};
+
+	std::string	get_header_for_msg(RequestType type, std::string const& msg = "")
+	{
+		const uint32_t magic_value = 0x53545259;
+		const uint16_t payload_length = msg.size();
+		const uint16_t request_code = static_cast<uint16_t>(type);
+
+		const std::size_t size_to_allocate = sizeof(magic_value) + sizeof(payload_length) + sizeof(request_code) + 1;
+
+		char *header = new char(size_to_allocate);
+		std::memset((void *)header, 0, size_to_allocate);
+
+		std::memcpy((void *)header, (void *)&magic_value, 4);
+		std::memcpy((void *)(header + 4), (void *)&payload_length, 2);
+		std::memcpy((void *)(header + 6), (void *)&request_code, 2);
+
+		std::string_view view(header, 6);
+
+		//delete header;
+		return std::string{view};
+	}
+} // namespace
 
 /*
 **	public methods
@@ -13,7 +46,27 @@ void Client::connect(boost::asio::ip::tcp::endpoint const& address)
 
 void Client::send_compress_request(std::string const& msg_to_compress)
 {
-	_send_msg(msg_to_compress);
+	std::string const& msg_header = get_header_for_msg(RequestType::compress, msg_to_compress);
+
+	std::cout << "header size: " << msg_header.size() << std::endl;
+	std::cout << "msg size: " << msg_to_compress.size() << std::endl;
+
+	_send_msg(msg_header + msg_to_compress);
+}
+
+void Client::send_get_stats_request()
+{
+	_send_msg(get_header_for_msg(RequestType::get_stats));
+}
+
+void Client::send_ping_request()
+{
+	_send_msg(get_header_for_msg(RequestType::ping));
+}
+
+void Client::send_reset_stats_request()
+{
+	_send_msg(get_header_for_msg(RequestType::reset_stats));
 }
 
 std::string Client::receive_msg()
@@ -22,7 +75,7 @@ std::string Client::receive_msg()
 
 	msg = _get_server_responce();
 
-	return msg;
+	return std::move(msg);
 }
 
 /*
@@ -31,6 +84,8 @@ std::string Client::receive_msg()
 
 void	Client::_send_msg(std::string const& msg)
 {
+	std::cout << "total msg size: " << msg.size() << std::endl;
+
 	_sock.write_some(boost::asio::buffer(msg, msg.size()));
 }
 
@@ -39,25 +94,10 @@ std::string Client::_get_server_responce()
 	std::string responce;
 
 	try {
-		// boost::system::error_code error;
-		// boost::asio::streambuf receive_buffer;
-
-		// boost::asio::read(_sock, receive_buffer, boost::asio::transfer_all(), error);
-		// _sock.read_some(receive_buffer);
-
 		char buff[1024]{};
 
 		std::size_t len = _sock.read_some(boost::asio::buffer(buff));
 		responce = buff;
-
-		// if (false && error && error != boost::asio::error::eof)
-		// {
-		// 	std::cout << "Error. receive failed: " << error.message() << std::endl;
-		// }
-		// else
-		// {
-		// 	responce = boost::asio::buffer_cast<char const*>(receive_buffer.data());
-		// }
 	} catch (std::exception e)
 	{
 		std::cerr << "Exception: " << e.what() << std::endl;
