5b9eb3328c31d47b0e79340d7f2a6c620dbdca2d
apavlyuc <apavlyuc@gmail.com>
2020-06-28 00:59:59 +0300

    compressor throw if get nullptr; use fill_n & append_block lambda

--- server/src/Compressors.cpp
+++ server/src/Compressors.cpp
@@ -1,5 +1,7 @@
 #include <Compressors.hpp>
 
+#include <algorithm>
+#include <iterator>
 #include <stdexcept>
 
 namespace compressors {
@@ -7,6 +9,11 @@ namespace compressors {
 	namespace {
 		void throw_if_invalid_text(char const* text, std::size_t text_len)
 		{
+			if (!text || !text_len)
+			{
+				throw std::runtime_error("compressor get empty text(nullptr or 0 as a len).");
+			}
+
 			auto is_digit = [](char c) {
 				return (c >= '0' && c <= '9');
 			};
@@ -32,18 +39,26 @@ namespace compressors {
 
 	std::string	default_compress(char const* text, std::size_t text_len)
 	{
-		if (text == nullptr || text_len == 0)
-		{
-			return "";
-		}
-
 		throw_if_invalid_text(text, text_len);
 
-		std::string ret;
-		ret.reserve(text_len);
+		std::string compressed_text;
+		compressed_text.reserve(text_len);
 
-		std::size_t count_of_repeatings{1};
+		auto append_block = [](std::string& dst, char symbol, std::size_t symbol_repeating){
+			if (symbol_repeating < 3)
+			{
+				std::back_insert_iterator<std::string> back_insert_it(dst);
+				std::fill_n(back_insert_it, symbol_repeating, symbol);
+			}
+			else
+			{
+				dst += std::to_string(symbol_repeating);
+				dst += symbol;
+			}
+		};
+
 		char current_symbol = text[0];
+		std::size_t count_of_repeatings = 1;
 		for (std::size_t i = 1; i < text_len; ++i)
 		{
 			if (text[i] == current_symbol)
@@ -52,39 +67,14 @@ namespace compressors {
 				continue;
 			}
 
-			if (count_of_repeatings == 1)
-			{
-				ret += current_symbol;
-			}
-			else if (count_of_repeatings == 2)
-			{
-				ret += current_symbol;
-				ret += current_symbol;
-			}
-			else
-			{
-				ret += std::to_string(count_of_repeatings);
-				ret += current_symbol;
-			}
+			append_block(compressed_text, current_symbol, count_of_repeatings);
+
 			current_symbol = text[i];
 			count_of_repeatings = 1;
 		}
 
-		if (count_of_repeatings == 1)
-		{
-			ret += current_symbol;
-		}
-		else if (count_of_repeatings == 2)
-		{
-			ret += current_symbol;
-			ret += current_symbol;
-		}
-		else
-		{
-			ret += std::to_string(count_of_repeatings);
-			ret += current_symbol;
-		}
+		append_block(compressed_text, current_symbol, count_of_repeatings);
 
-		return std::move(ret);
+		return std::move(compressed_text);
 	}
 };
