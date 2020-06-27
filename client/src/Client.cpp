19c9e866df854bb0d784b24b795f73fd55a22483
apavlyuc <apavlyuc@gmail.com>
2020-06-28 01:24:28 +0300

    add header parsing to determin what status code is; print it into stdout

--- client/src/Client.cpp
+++ client/src/Client.cpp
@@ -39,6 +39,50 @@ std::string Client::receive_msg()
 
 	msg = _get_server_responce();
 
+	if (msg.empty() == false)
+	{
+		protocol::Default::StatusCode status = protocol::Default::get_status_code(msg.c_str());
+
+		switch (status)
+		{
+			case protocol::Default::StatusCode::unknown_error:
+			{
+				std::cerr << "server status: unknown_error" << std::endl;
+				msg.clear();
+				break;
+			}
+			case protocol::Default::StatusCode::message_too_large:
+			{
+				std::cerr << "server status: message_too_large" << std::endl;
+				msg.clear();
+				break;
+			}
+			case protocol::Default::StatusCode::unsupported_request_type:
+			{
+				std::cerr << "server status: unsupported_request_type" << std::endl;
+				msg.clear();
+				break;
+			}
+			case protocol::Default::StatusCode::message_contains_digits:
+			{
+				std::cerr << "server status: message_contains_digits" << std::endl;
+				msg.clear();
+				break;
+			}
+			case protocol::Default::StatusCode::message_contains_uppercase:
+			{
+				msg.clear();
+				std::cerr << "server status: message_contains_uppercase" << std::endl;
+				break;
+			}
+			default:
+			{
+				std::cout << "server status: ok" << std::endl;
+				msg = msg.substr(protocol::Default::header_len);
+			}
+		}
+	}
+
 	return std::move(msg);
 }
 
@@ -71,7 +115,6 @@ std::string Client::_get_server_responce()
 	std::string ret;
 
 	char *header = nullptr;
-	
 	char *payload = nullptr;
 	std::size_t payload_len = 0;
 	try {
@@ -79,12 +122,12 @@ std::string Client::_get_server_responce()
 		boost::asio::read(_m_sock, boost::asio::buffer(header, protocol::Default::header_len));
 
 		payload_len = protocol::Default::get_payload_length(header);
-		std::cout << "payload_len: " << payload_len << std::endl;
 
 		payload = new char[payload_len];
 		boost::asio::read(_m_sock, boost::asio::buffer(payload, payload_len));
 
-		ret = std::string(payload, payload_len);
+		ret = std::string(header, protocol::Default::header_len);
+		ret += std::string(payload, payload_len);
 
 	} catch (std::exception e)
 	{
